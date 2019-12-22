4017ea5ca82ec282d82b5ea0b9e9ea7821071a66
Ryder Casazza <rydercasazza@me.com>
2019-12-22 12:29:10 -0800

    Finished binary printer stuck on base64decoder

--- src/_03_Printing_Binary/BinaryPrinter.java
+++ src/_03_Printing_Binary/BinaryPrinter.java
@@ -1,25 +1,59 @@
 package _03_Printing_Binary;
 
 public class BinaryPrinter {
+	public static void main(String[] args) {
+		System.out.println(Integer.MAX_VALUE);
+		 BinaryPrinter bp = new BinaryPrinter();
+		 bp.printLongBinary(34);
+		 System.out.println("\n");
+		 bp.printByteBinary((byte) 34);
+		 System.out.println("\n");
+		 bp.printByteBinary((byte) 255);
+		 System.out.println("\n");
+		 bp.printByteBinary((byte) -1);
+		 System.out.println("\n");
+		 bp.printShortBinary((short) 34); 
+		 System.out.println("\n");
+		 bp.printIntBinary(Integer.MAX_VALUE);
+		 System.out.println("\n");
+		 bp.printIntBinary(-1);
+	}
 	//Complete the methods below so they print the passed in parameter in binary.
-	//Use bit shifting and bit masking to print the binary numbers.
-	//Do not use the Integer.toBinaryString method!
 	//Don't be afraid to use the methods that are already complete to finish the others.
 	//Create a main method to test your methods.
 	
 	public void printByteBinary(byte b) {
-	
+		for(int i = 128;i>0;i/=2) {
+			if((b&i)>0) {
+				System.out.print(1);
+			} else {
+				System.out.print(0);
+			}
+		}
 	}
 	
 	public void printShortBinary(short s) {
-		
+		for(int i = (int) Math.pow(2, 16);i>0;i/=2) {
+			if((s&i)>0) {
+				System.out.print(1);
+			} else {
+				System.out.print(0);
+			}
+		}
 	}
 	
 	public void printIntBinary(int i) {
-		
+		BinaryPrinter bp = new BinaryPrinter();
+		bp.printShortBinary((short)(((short)i&0b11111111111111110000000000000000)>>16));
+		bp.printShortBinary((short)i);
 	}
-	
+
 	public void printLongBinary(long l) {
-		
+		for(int i = (int)Math.pow(2, (int)(Math.log(l)/Math.log(2)));i > 0; i/=2) {
+			if ((l&i) > 0)
+				System.out.print(1);
+			else
+				System.out.print(0);
+		}
 	}
 }
